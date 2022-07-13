95dd82d8884187a8e70b029255bdc041139b4481
Vishal Ribdiya <vishal.ribdiya@infyom.com>
2022-07-13 15:49:02 +0530

    chore: missing menu template added

--- views/templates/layouts/menu_template.blade.php
+++ views/templates/layouts/menu_template.blade.php
@@ -2,7 +2,7 @@
     <a href="@{{ route('{!! $config->prefixes->getRoutePrefixWith('.') !!}{!! $config->modelNames->camelPlural !!}.index') }}" class="nav-link @{{ Request::is('{!! $config->prefixes->getRoutePrefixWith('.') !!}{!! $config->modelNames->camelPlural !!}*') ? 'active' : '' }}">
         <i class="nav-icon fas fa-home"></i>
 @if($config->options->localized)
-        <p>@{{ __('models/{{ $config->modelNames->camelPlural }}.singular') }}</p>
+        <p>@{{ __('models/'.{{ $config->modelNames->camelPlural }}.'singular') }}</p>
 @else
         <p>{{ $config->modelNames->humanPlural }}</p>
 @endif
