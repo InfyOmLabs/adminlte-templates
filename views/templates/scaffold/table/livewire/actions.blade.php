b81a60772512c8b4937d729e2c557f39090dc234
Vishal Ribdiya <vishal.ribdiya@infyom.com>
2022-07-15 16:42:46 +0530

    refactor: minor chnages into livewire actions

--- views/templates/scaffold/table/livewire/actions.blade.php
+++ views/templates/scaffold/table/livewire/actions.blade.php
@@ -1,8 +1,8 @@
 <div class='btn-group'>
-    <a href="@{{ $showUrl }}" class='btn btn-default btn-xs'>
+    <a href="{{ $showUrl }}" class='btn btn-default btn-xs'>
         <i class="fa fa-eye"></i>
     </a>
-    <a href="@{{ $editUrl }}" class='btn btn-default btn-xs'>
+    <a href="{{ $editUrl }}" class='btn btn-default btn-xs'>
         <i class="fa fa-edit"></i>
     </a>
     <a class='btn btn-danger btn-xs' wire:click="deleteRecord(@{{ $recordId }})"
