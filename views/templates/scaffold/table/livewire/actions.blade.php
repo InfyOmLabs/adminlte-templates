53771cf8fa15ae552c21ac06997fbf73a3dd4ee3
Vishal Ribdiya <33887755+vishalinfyom@users.noreply.github.com>
2022-07-16 10:58:28 +0530

    Vishal/fixes (#89)

--- views/templates/scaffold/table/livewire/actions.blade.php
+++ views/templates/scaffold/table/livewire/actions.blade.php
@@ -5,7 +5,7 @@
     <a href="{{ $editUrl }}" class='btn btn-default btn-xs'>
         <i class="fa fa-edit"></i>
     </a>
-    <a class='btn btn-danger btn-xs' wire:click="deleteRecord(@{{ $recordId }})"
+    <a class='btn btn-danger btn-xs' wire:click="deleteRecord({{ $recordId }})"
        onclick="confirm('Are you sure you want to remove this Record?') || event.stopImmediatePropagation()">
         <i class="fa fa-trash"></i>
     </a>
